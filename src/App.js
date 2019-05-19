09cbd7e41d9e12267380ea0fadcc046afb846ace
Eunwoo <dmsdn960@gmail.com>
2019-05-19 22:50:25 +0900

    add todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import ToDoList from './ToDoList';
+import ToDo from './ToDo';
 
 const Add = styled.button`
   width: 2rem;
@@ -29,16 +29,51 @@ const TextInput = styled.div`
 `
 
 export default class App extends Component {
+  state = {
+    newToDo: {
+      title: '',
+      content: '',
+      date: ''
+    },
+    todoList: []
+  }
   render() {
+    const { newToDo, todoList } = this.state;
+    const TodoList = todoList.map((value, index)=>{
+      return <ToDo key={index} title={value.title} content={value.content} dead={value.date} />;
+    })
     return (
       <div>
         <h1>ToDoList</h1>
-        <TextInput><Input type="text" placeholder="제목" /></TextInput>
-        <TextInput><Input type="text" placeholder="내용" /></TextInput>
-        <TextInput><Input type="date" /></TextInput>
-        <Add>+</Add>
-        <ToDoList></ToDoList>
+        <TextInput><Input type="text" name="title" placeholder="제목" value={newToDo.title} onChange={this._handleChange}/></TextInput>
+        <TextInput><Input type="text" name="content" placeholder="내용" value={newToDo.content} onChange={this._handleChange}/></TextInput>
+        <TextInput><Input type="date" name="date" value={newToDo.date} onChange={this._handleChange}/></TextInput>
+        <Add onClick={this._addToDo}>+</Add>
+        {TodoList}
       </div>
     )
+  }
+  _handleChange = (e) => {
+    this.setState({
+      newToDo: {
+        ...this.state.newToDo,
+        [e.target.name]: e.target.value
+      }
+    });
+  }
+  _addToDo = () => {
+    const { todoList, newToDo } = this.state;
+    this.setState({
+      todoList: [
+        ...todoList,
+        newToDo
+      ]
+    })
+  }
+  _loadToDo = () => {
+
+  }
+  _deleteToDo = () => {
+    
   }
 }
