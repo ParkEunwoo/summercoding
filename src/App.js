f7536e2128914388d1abfd87bdb60403c2b9e681
Eunwoo <dmsdn960@gmail.com>
2019-05-19 19:37:03 +0900

    todolist container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ToDoList from './ToDoList';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <h1>ToDoList</h1>
+        <input type="text"></input>
+        <button>추가</button>
+        <ToDoList></ToDoList>
       </div>
     )
   }
